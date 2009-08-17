78567e07c25d795fd88db1d6ce666cadf3cda0c1
unknown <Corion@.(none)>
2009-08-17 12:34:30 +0200

    Added 'archive' option

--- bin/import-images.pl
+++ bin/import-images.pl
@@ -7,7 +7,7 @@ use Data::Dumper;
 use File::Glob qw(bsd_glob);
 use File::Basename qw(basename);
 use File::Spec;
-use File::Copy qw(cp);
+use File::Copy qw(cp move);
 use Memoize qw(memoize);
 
 use Getopt::Long;
@@ -15,10 +15,15 @@ use Pod::Usage;
 
 GetOptions(
     'target|t=s' => \my $target,
+    'archive|a'  => \my $archive_dir,
+    'verbose|v' => \my $verbose,
     'buffer-size|b=i' => \my $bufsize,
 ) or pod2usage(1);
 
 $bufsize ||= 16384 * 1024 * 1024;
+if ($archive_dir) {
+    $archive_dir = 'archive';
+};
 
 sub take($;@) {
     my $list = shift;
@@ -52,21 +57,55 @@ print "Reading files";
 
 if (! @ARGV) {
     @ARGV = qw(
-        E:/DCIM/CANON100
-        D:/DCIM/CASIO100
+        E:/DCIM/*
+        D:/DCIM/*
     );
+};
+
+if ($verbose) {
+    local $" = ",";
+    print " @ARGV";
+}
+
+sub archive_dir {
+    my ($file) =  @_;
+    if ($archive_dir) {
+        my $dir = dirname $file;
+        my $adir = File::Spec->catdir($dir,$archive_dir);
+        if( -d && ! -d $dir) {
+            mkdir $dir
+                or warn "Couldn't create archive directory '$dir'";
+            return undef
+        };
+        return $adir
+    }
+    return undef
+}
+memoize('archive_dir');
+
+sub archive_file {
+    my ($file) = @_;
+    if (defined( my $archive = archive_dir($file))) {
+        move $_[0] => $archive
+            or warn "Couldn't archive $_[0]: $!";
+    }
 }
 
 my %c;
 my @files = sort { capture_date($a) <=> capture_date($b) }
             #take_first 3,
-            map  { glob "$_/*" } @ARGV;
+            grep { -f }
+            map  { bsd_glob "$_/*" } @ARGV;
 
 # Images taken 5 hours apart get a new directory:
 my $distance = DateTime::Duration->new( hours => 5  );
 my $reference = DateTime->now;
 
 printf ", found %s unsorted images\n", scalar @files;
+
+if ($verbose) {
+    print "Copying to $target\n";
+}
 
 my $last_time = DateTime->from_epoch( epoch => 1 );
 my $target_directory;
@@ -79,13 +118,13 @@ for my $image (@files) {
         }
     };
     $last_time = capture_date($image);
-    # and move the files into their newly found location:
+    # and copy the files into their newly found location:
     my $base = basename $image;
     my $target_name = File::Spec->catfile($target_directory, $base);
-    #print "$image => $target_name\n";
     if (-f $target_name) {
         warn "$target_name already exists, skipped.\n";
     } else {
         cp $image => $target_name, $bufsize;
+        archive_file( $image );
     }
 };
